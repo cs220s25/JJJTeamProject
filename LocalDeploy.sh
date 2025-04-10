af08c3a280b95da81145bcd24bd42a7b09b7e979
Jack Wagenheim <wagenheimj@moravian.edu>
2025-04-10 19:23:40 -0400

    Made LocalDeploy work without preinstalled stuff

--- LocalDeploy.sh
+++ LocalDeploy.sh
@@ -1,3 +1,10 @@
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install maven and redis
+brew install maven
+brew install redis
+
 # Starts redis server on port 6379 with localhost
 # Start redis-server as a background process (daemon)
 brew services start redis
@@ -7,6 +14,6 @@ mvn clean
 mvn package
 
 # Launch server 
-java -jar target/dbot-1.0-SNAPSHOT.jar
+]java -jar target/dbot-1.0-SNAPSHOT.jar
 
 
