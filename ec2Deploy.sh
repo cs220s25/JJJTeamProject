6dc007a1317287e1db77cd767a59791932728360
Jack Wagenheim <wagenheimj@moravian.edu>
2025-04-10 17:32:31 -0400

    Changed from mvnw to mvn in ec2Deploy.sh

--- ec2Deploy.sh
+++ ec2Deploy.sh
@@ -1,6 +1,6 @@
 # run Build
-JJJTeamProject/mvnw clean -f JJJTeamProject/pom.xml 
-JJJTeamProject/mvnw package -f JJJTeamProject/pom.xml
+mvn clean -f JJJTeamProject/pom.xml 
+mvn package -f JJJTeamProject/pom.xml
 
 # Start application from .jar
 java -jar JJJTeamProject/target/dbot-1.0-SNAPSHOT.jar "./JJJTeamProject/"
