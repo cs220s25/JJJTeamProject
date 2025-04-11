b12a8e697710db80fc1a33da6684fd4b5ecb62bf
Jon Walsh <walshj05@moravian.edu>
2025-04-11 18:35:07 -0400

    Removed old shell scripts for ec2

--- ec2Deploy.sh
+++
@@ -1,6 +0,0 @@
-# run Build
-mvn clean -f JJJTeamProject/pom.xml 
-mvn package -f JJJTeamProject/pom.xml
-
-# Start application from .jar
-java -jar JJJTeamProject/target/dbot-1.0-SNAPSHOT.jar "./JJJTeamProject/"
