69eb9d4b08fde484aab4fc938033c5a6c1edbbac
Jamell Alvarez <alvarezj@moravian.edu>
2025-04-11 18:56:45 -0400

    Changed LocalDeploy script so it doesnt install anything

--- LocalDeploy.sh
+++ LocalDeploy.sh
@@ -1,19 +1,13 @@
-# Install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Install maven and redis
-brew install maven
-brew install redis
-
-# Starts redis server on port 6379 with localhost
 # Start redis-server as a background process (daemon)
 brew services start redis
+
+# Current Directory is the repo
+cd /JJJTeamProject
 
 # Maven lifecycle
 mvn clean
 mvn package
 
-# Launch server 
-]java -jar target/dbot-1.0-SNAPSHOT.jar
-
-
+# Launch server
+java -jar target/dbot-1.0-SNAPSHOT.jar
